b5effab2386d2363212604bf8f9d14c3e7373022
VanKorno <kornovandrey@gmail.com>
2024-10-23 16:26:08 +0300

    Started with themes

---
+++ VanKornoCompose/src/main/java/com/vankorno/vankornocompose/shared/TopographyNunito.kt
@@ -0,0 +1,92 @@
+package com.vankorno.vankornocompose.shared
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.vankorno.vankornocompose.R
+
+val FontFNunito = FontFamily(
+    Font(R.font.nunito_extralight, FontWeight.ExtraLight),
+    Font(R.font.nunito_light, FontWeight.Light),
+    Font(R.font.nunito_regular, FontWeight.Normal),
+    Font(R.font.nunito_medium, FontWeight.Medium),
+    Font(R.font.nunito_semibold, FontWeight.SemiBold),
+    Font(R.font.nunito_bold, FontWeight.Bold),
+    Font(R.font.nunito_extrabold, FontWeight.ExtraBold),
+    Font(R.font.nunito_black, FontWeight.Black)
+)
+
+
+val TopographyNunito = Typography(
+    
+    titleLarge = TextStyle(
+        fontFamily = FontFNunito,
+        fontWeight = FontWeight.Normal,
+        fontSize = 28.sp,
+        lineHeight = 34.sp,
+        letterSpacing = 0.7.sp
+    )
+    ,titleMedium = TextStyle(
+        fontFamily = FontFNunito,
+        fontWeight = FontWeight.Normal,
+        fontSize = 26.sp,
+        lineHeight = 32.sp,
+        letterSpacing = 0.7.sp
+    )
+    ,titleSmall = TextStyle(
+        fontFamily = FontFNunito,
+        fontWeight = FontWeight.Normal,
+        fontSize = 24.sp,
+        lineHeight = 30.sp,
+        letterSpacing = 0.7.sp
+    )
+    
+    ,bodyLarge = TextStyle(
+        fontFamily = FontFNunito,
+        fontWeight = FontWeight.Bold,
+        fontSize = 22.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.7.sp
+    )
+    
+    ,bodyMedium = TextStyle(
+        fontFamily = FontFNunito,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp,
+        lineHeight = 26.sp,
+        letterSpacing = 0.7.sp
+    )
+    ,bodySmall = TextStyle(
+        fontFamily = FontFNunito,
+        fontWeight = FontWeight.Bold,
+        fontSize = 18.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.7.sp
+    )
+    
+    
+    ,labelLarge = TextStyle(
+        fontFamily = FontFNunito,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp,
+        lineHeight = 22.sp,
+        letterSpacing = 0.7.sp
+    )
+    ,labelMedium = TextStyle(
+        fontFamily = FontFNunito,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.7.sp
+    )
+    ,labelSmall = TextStyle(
+        fontFamily = FontFNunito,
+        fontWeight = FontWeight.Medium,
+        fontSize = 12.sp,
+        lineHeight = 18.sp,
+        letterSpacing = 0.7.sp
+    )
+)
