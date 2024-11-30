880bb0a89e306a0f0a9250ef8d742e84557e5c4c
VanKorno <kornovandrey@gmail.com>
2024-11-30 02:48:39 +0200

    Added .orLessSp()

--- VanKornoCompose/build.gradle.kts
+++ VanKornoCompose/build.gradle.kts
@@ -71,7 +71,7 @@ dependencies {
     implementation(libs.vankornohelpers)
 }
 
-val versionTag = "2.0.4"
+val versionTag = "2.0.5"
 
 publishing {    /* to ensure that the library is published correctly */
     publications {
--- VanKornoCompose/src/main/java/com/vankorno/vankornocompose/actions/LibComposeExtFun.kt
+++ VanKornoCompose/src/main/java/com/vankorno/vankornocompose/actions/LibComposeExtFun.kt
@@ -3,22 +3,23 @@ package com.vankorno.vankornocompose.actions
 import android.util.Log
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
+import com.vankorno.vankornocompose.LibScreen.Companion.microUI
+import com.vankorno.vankornocompose.LibScreen.Companion.smallUI
 import kotlinx.coroutines.flow.StateFlow
 
 
 fun Color.tweakTransparency(opacity: Float) = Color(this.red, this.green, this.blue, opacity)
 
 
-fun StateFlow<String>.toNoNullInt(                                               default: Int = 0
-) = try {
-        this.value.toInt()
-    } catch (nfe:NumberFormatException) {
-        default
-    }
+fun StateFlow<String>.toNoNullInt(default: Int = 0) = try {
+                                                        this.value.toInt()
+                                                    } catch (nfe:NumberFormatException) {
+                                                        default
+                                                    }
 
 
-
-fun FocusRequester.aaakRequest() =  try {
+fun FocusRequester.libRequest() =  try {
                                         this.requestFocus()
                                         true
                                     } catch (e: Exception) {
@@ -28,3 +29,12 @@ fun FocusRequester.aaakRequest() =  try {
                                         false
                                     }
 
+
+fun Int.orLessSp(fontSize: Int) =   if (fontSize <= 7)
+                                        fontSize
+                                    else if (microUI)
+                                        (fontSize - 4).sp
+                                    else if (smallUI)
+                                        (fontSize - 2).sp
+                                    else
+                                        fontSize
