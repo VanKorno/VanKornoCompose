a5f33abd55b5a42ac6e0a818147e18b95bd49ce2
VanKorno <71512024+vankorno@users.noreply.github.com>
2025-07-03 20:33:57 +0300

    ...

--- VanKornoCompose/src/main/java/com/vankorno/vankornocompose/composables/LibIcons.kt
+++ VanKornoCompose/src/main/java/com/vankorno/vankornocompose/composables/LibIcons.kt
@@ -8,20 +8,50 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.vankorno.vankornocompose.R
 import com.vankorno.vankornohelpers.LibUI
 
 private val MOD_CheckCircle = Modifier.size(37.dp).padding(6.dp)
 
 
 @Composable
-fun LibCheckCircleIcon(                                        isPicked: Boolean,
+fun LibCheckCircleIcon(                                          picked: Boolean,
                                                                modifier: Modifier = MOD_CheckCircle,
                                                                   color: Color = Color.White
 ) {
     Icon(
-            modifier = modifier,
-            painter = painterResource(id = LibUI().getCircleIcon(isPicked)),
-            tint = color,
-            contentDescription = null
-        )
+        modifier = modifier,
+        painter = painterResource(id = LibUI().getCircleIcon(picked)),
+        tint = color,
+        contentDescription = null
+    )
 }
+
+
+
+@Composable
+fun LibPinIcon(                                                  pinned: Boolean,
+                                                               modifier: Modifier = MOD_CheckCircle,
+                                                                  color: Color = Color.White
+) {
+    Icon(
+        modifier = modifier,
+        painter = painterResource(
+            id = if (pinned)
+                R.drawable.ic_pin_filled
+            else
+                R.drawable.ic_pin
+        ),
+        tint = color,
+        contentDescription = null
+    )
+}
+
+
+
+
+
+
+
+
+
