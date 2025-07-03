09c7e092a24f6579954774deebfccf599282cf90
VanKorno <71512024+vankorno@users.noreply.github.com>
2025-07-03 20:08:43 +0300

    ...

--- VanKornoCompose/src/main/java/com/vankorno/vankornocompose/composables/LibIcons.kt
+++ VanKornoCompose/src/main/java/com/vankorno/vankornocompose/composables/LibIcons.kt
@@ -1,19 +1,27 @@
 package com.vankorno.vankornocompose.composables
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import com.vankorno.vankornocompose.values.MOD_CheckCircle
+import androidx.compose.ui.unit.dp
 import com.vankorno.vankornohelpers.LibUI
 
+private val MOD_CheckCircle = Modifier.size(37.dp).padding(6.dp)
+
+
 @Composable
-fun LibCheckCircleIcon(                                                         isPicked: Boolean
+fun LibCheckCircleIcon(                                        isPicked: Boolean,
+                                                               modifier: Modifier = MOD_CheckCircle,
+                                                                  color: Color = Color.White
 ) {
     Icon(
-            modifier = MOD_CheckCircle,
+            modifier = modifier,
             painter = painterResource(id = LibUI().getCircleIcon(isPicked)),
-            tint = Color.White,
+            tint = color,
             contentDescription = null
         )
 }
--- VanKornoCompose/src/main/java/com/vankorno/vankornocompose/values/LibModifiers.kt
+++ VanKornoCompose/src/main/java/com/vankorno/vankornocompose/values/LibModifiers.kt
@@ -4,8 +4,6 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
@@ -18,4 +16,3 @@ val MOD_W50 = Modifier.fillMaxWidth(0.5f)
 
 val MOD_DefaultSpacer = Modifier.height(35.dp)
 
-val MOD_CheckCircle = Modifier.size(37.dp).padding(6.dp)
