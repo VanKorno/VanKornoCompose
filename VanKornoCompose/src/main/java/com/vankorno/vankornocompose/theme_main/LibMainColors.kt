0afdf17ae6b3724a2d96dbec2a0c5f8600ba1568
VanKorno <kornovandrey@gmail.com>
2025-11-17 00:55:00 +0200

    Making the main theme

--- VanKornoCompose/src/main/java/com/vankorno/vankornocompose/theme_main/LibMainColors.kt
+++ VanKornoCompose/src/main/java/com/vankorno/vankornocompose/theme_main/LibMainColors.kt
@@ -1,28 +1,33 @@
 package com.vankorno.vankornocompose.theme_main
 
 import androidx.compose.ui.graphics.Color
-import com.vankorno.vankornohelpers.values.LibColors
 
-//  D A R K    M O D E
-val LibColorBackgroundD = Color(0xFF242424)
+val LibColorBackground = Color(0xFF242424)
 
-val LibColorWhiteTextD = Color(0xFFE6E6E6)
+val LibColorWhiteText = Color(0xFFE6E6E6)
 
-val LibColorBlackBtnD = Color(0xFF000000)
-val LibColorBlackBtnDisabledD = Color(0x4D000000)
+val LibColorBlackBtn = Color(0xFF000000)
+val LibColorBlackBtnDisabled = Color(0x4D000000)
 
-val LibColorBlackBtnBorderD = Color(0xFF5A5A5A)
-val LibColorBlackBtnBorderDisabledD = Color(0x1AFFFFFF)
+val LibColorBlackBtnBorder = Color(0xFF5A5A5A)
+val LibColorBlackBtnBorderDisabled = Color(0x1AFFFFFF)
+
+
 
 val LibColorRedM800    = Color(0xFFC62828)
 val LibColorPinkM800   = Color(0xFFAD1457)
 val LibColorMagentaM800  = Color(0xFF6A1B9A)
 val LibColorVioletM800  = Color(0xFF4527A0)
 val LibColorBlueM800     = Color(0xFF283593)
+
 val LibColorLightBlueM800    = Color(0xFF0277BD)
+
 val LibColorGreenM800   = Color(0xFF00695C)
+
 val LibColorLightGreenM800   = Color(0xFF558B2F)
+
 val LibColorYellowM800    = Color(0xFFF9A825)
+
 val LibColorPumpkinM800    = Color(0xFFEF6C00)
 val LibColorOrangeM800    = Color(0xFFD84315)
 
@@ -30,29 +35,6 @@ val LibColorOrangeM800    = Color(0xFFD84315)
 
 
 
-//  L I G H T    M O D E
-val LibColorBackgroundL = Color(0xFF5C5C5D)
-
-val LibColorWhiteTextL = Color(0xFFFFFFFF)
-
-val LibColorBlackBtnL = Color(0xFF242424)
-val LibColorBlackBtnDisabledL = Color(0x4D242424)
-
-val LibColorBlackBtnBorderL = Color(0xFFBABABA)
-val LibColorBlackBtnBorderDisabledL = Color(0x1ABABABA)
-
-
-val LibColorRedM500    = Color(0xFFF44336)
-val LibColorPinkM500   = Color(0xFFE91E63)
-val LibColorMagentaM500  = Color(0xFF9C27B0)
-val LibColorVioletM500  = Color(0xFF673AB7)
-val LibColorBlueM500     = Color(0xFF3F51B5)
-val LibColorLightBlueM500    = Color(0xFF03A9F4)
-val LibColorGreenM500   = Color(0xFF009688)
-val LibColorLightGreenM500   = Color(0xFF4CAF50)
-val LibColorYellowM500    = Color(0xFFFFEB3B)
-val LibColorPumpkinM500    = Color(0xFFFF9800)
-val LibColorOrangeM500    = Color(0xFFFF5722)
 
 
 
@@ -63,8 +45,3 @@ val LibColorOrangeM500    = Color(0xFFFF5722)
 
 
 
-
-
-
-
-val PlainGreyMild    = Color(LibColors.PlainDarkGrey)
