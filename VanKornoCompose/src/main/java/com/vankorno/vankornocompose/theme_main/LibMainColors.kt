be610855a46539980c9d48712274f97659ae1045
VanKorno <kornovandrey@gmail.com>
2025-11-17 00:39:24 +0200

    Making the main theme

--- VanKornoCompose/src/main/java/com/vankorno/vankornocompose/theme_main/LibMainColors.kt
+++ VanKornoCompose/src/main/java/com/vankorno/vankornocompose/theme_main/LibMainColors.kt
@@ -14,6 +14,20 @@ val LibColorBlackBtnDisabledD = Color(0x4D000000)
 val LibColorBlackBtnBorderD = Color(0xFF5A5A5A)
 val LibColorBlackBtnBorderDisabledD = Color(0x1AFFFFFF)
 
+val LibColorRedM800    = Color(0xFFC62828)
+val LibColorPinkM800   = Color(0xFFAD1457)
+val LibColorMagentaM800  = Color(0xFF9C27B0)
+val LibColorVioletM800  = Color(0xFF673AB7)
+val LibColorBlueM800     = Color(0xFF3F51B5)
+val LibColorLightBlueM800    = Color(0xFF2196F3)
+val LibColorGreenM800   = Color(0xFF009688)
+val LibColorLightGreenM800   = Color(0xFF4CAF50)
+val LibColorYellowM800    = Color(0xFFFFEB3B)
+val LibColorPumpkinM800    = Color(0xFFFF9800)
+val LibColorOrangeM800    = Color(0xFFFF5722)
+
+
+
 
 
 //  L I G H T    M O D E
@@ -28,6 +42,24 @@ val LibColorBlackBtnBorderL = Color(0xFFBABABA)
 val LibColorBlackBtnBorderDisabledL = Color(0x1ABABABA)
 
 
+val LibColorRedM500    = Color(0xFFF44336)
+val LibColorPinkM500   = Color(0xFFE91E63)
+val LibColorMagentaM500  = Color(0xFF9C27B0)
+val LibColorVioletM500  = Color(0xFF673AB7)
+val LibColorBlueM500     = Color(0xFF3F51B5)
+val LibColorLightBlueM500    = Color(0xFF2196F3)
+val LibColorGreenM500   = Color(0xFF009688)
+val LibColorLightGreenM500   = Color(0xFF4CAF50)
+val LibColorYellowM500    = Color(0xFFFFEB3B)
+val LibColorPumpkinM500    = Color(0xFFFF9800)
+val LibColorOrangeM500    = Color(0xFFFF5722)
+
+
+
+
+
+
+
 
 
 
@@ -36,15 +68,3 @@ val LibColorBlackBtnBorderDisabledL = Color(0x1ABABABA)
 
 
 val PlainGreyMild    = Color(LibColors.PlainDarkGrey)
-val PlainBlueMild    = Color(0xFF5278A6)
-val PlainGreenMild   = Color(0xFF4F7F58)
-val PlainYellowMild  = Color(0xFF9B8F47)
-val PlainOrangeMild  = Color(0xFF9B623F)
-val PlainRedMild     = Color(0xFF8A4A42)
-val PlainPinkMild    = Color(0xFF8A4A66)
-val PlainPurpleMild  = Color(0xFF5C4A8A)
-val PlainTealMild    = Color(0xFF4A8A82)
-val PlainOliveMild   = Color(0xFF6D743F)
-val PlainBrownMild   = Color(0xFF6E5B45)
-val PlainAvocadoMild = Color(0xFF768A42)
-val PlainCyanMild    = Color(0xFF4A8A9B)
