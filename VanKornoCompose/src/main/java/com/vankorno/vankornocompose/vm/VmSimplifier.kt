505f9e06abd8ed1f934d1d220528f2e49b32afb7
VanKorno <71512024+VanKorno@users.noreply.github.com>
2026-02-09 13:20:46 +0200

    Update, extend VmSimplifier

--- VanKornoCompose/src/main/java/com/vankorno/vankornocompose/vm/VmSimplifier.kt
+++ VanKornoCompose/src/main/java/com/vankorno/vankornocompose/vm/VmSimplifier.kt
@@ -5,13 +5,21 @@ import androidx.compose.runtime.State
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+interface VmValueHolder<T> {
+    var value: T
+    val flow: StateFlow<T>
+}
+
 
 class VmVal<T>(                                                        default: T,
                                                              private val onSet: ((T)->Unit)? = null,
-) {
-    val flow = MutableStateFlow(default)
+) : VmValueHolder<T> {
 
-    var value: T
+    override val flow = MutableStateFlow(default)
+
+    override var value: T
         get() = flow.value
         set(new) {
             flow.value = new
@@ -19,14 +27,17 @@ class VmVal<T>(                                                        default: 
         }
 }
 
+
+
 class VmSavedVal<T>(                                                   val ssh: SavedStateHandle,
                                                                        val key: String,
                                                                        default: T,
                                                              private val onSet: ((T)->Unit)? = null,
-) {
-    val flow = MutableStateFlow(ssh[key] ?: default)
-    
-    var value: T
+) : VmValueHolder<T> {
+
+    override val flow = MutableStateFlow(ssh[key] ?: default)
+
+    override var value: T
         get() = flow.value
         set(new) {
             flow.value = new
@@ -36,8 +47,23 @@ class VmSavedVal<T>(                                                   val ssh: 
 }
 
 
-@Composable
-fun <T> VmVal<T>.state(): State<T> = flow.collectAsStateWithLifecycle()
 
-@Composable
-fun <T> VmSavedVal<T>.state(): State<T> = flow.collectAsStateWithLifecycle()
+@Composable fun <T> VmValueHolder<T>.state(): State<T> = flow.collectAsStateWithLifecycle()
+
+inline fun <T> VmValueHolder<T>.update(block: (T) -> T) { value = block(value) }
+
+fun VmValueHolder<Boolean>.flip() { value = !value }
+
+fun <T> VmValueHolder<T?>.clear() { value = null }
+
+fun VmValueHolder<Int>.inc() { value++ }
+fun VmValueHolder<Int>.dec() { value-- }
+
+fun <T> VmValueHolder<List<T>>.add(item: T) { value = value + item }
+fun <T> VmValueHolder<List<T>>.remove(item: T) { value = value - item }
+
+
+
+
+
+
