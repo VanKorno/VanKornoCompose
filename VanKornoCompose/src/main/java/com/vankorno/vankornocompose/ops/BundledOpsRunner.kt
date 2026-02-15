e8c1ace2ad4955a1d92c9b0ab66f2b090f3840f4
VanKorno <71512024+VanKorno@users.noreply.github.com>
2026-02-15 22:29:25 +0200

    OPS: remove bundled getters

--- VanKornoCompose/src/main/java/com/vankorno/vankornocompose/ops/BundledOpsRunner.kt
+++ VanKornoCompose/src/main/java/com/vankorno/vankornocompose/ops/BundledOpsRunner.kt
@@ -2,8 +2,8 @@ package com.vankorno.vankornocompose.ops
 
 import com.vankorno.vankornodb.api.DbLock
 
-class BundledOpsRunner(val lock: DbLock) {
-
+class BundledOpsRunner(                                                         val lock: DbLock
+) {
     // ======================= Exec ======================= \\
     fun exec(funName: String = "exec", block: () -> Unit) = ExecOp0(lock, funName, block)
     fun <P1> exec(funName: String = "exec", block: (P1) -> Unit) = ExecOp1(lock, funName, block)
@@ -16,17 +16,4 @@ class BundledOpsRunner(val lock: DbLock) {
     fun <P1, P2, P3, P4, P5, P6, P7, P8> exec(funName: String = "exec", block: (P1, P2, P3, P4, P5, P6, P7, P8) -> Unit) = ExecOp8(lock, funName, block)
     fun <P1, P2, P3, P4, P5, P6, P7, P8, P9> exec(funName: String = "exec", block: (P1, P2, P3, P4, P5, P6, P7, P8, P9) -> Unit) = ExecOp9(lock, funName, block)
     fun <P1, P2, P3, P4, P5, P6, P7, P8, P9, P10> exec(funName: String = "exec", block: (P1, P2, P3, P4, P5, P6, P7, P8, P9, P10) -> Unit) = ExecOp10(lock, funName, block)
-
-    // ======================= Get ======================= \\
-    fun <R> get(default: R, funName: String = "get", block: () -> R) = GetOp0(lock, default, funName, block)
-    fun <P1, R> get(default: R, funName: String = "get", block: (P1) -> R) = GetOp1(lock, default, funName, block)
-    fun <P1, P2, R> get(default: R, funName: String = "get", block: (P1, P2) -> R) = GetOp2(lock, default, funName, block)
-    fun <P1, P2, P3, R> get(default: R, funName: String = "get", block: (P1, P2, P3) -> R) = GetOp3(lock, default, funName, block)
-    fun <P1, P2, P3, P4, R> get(default: R, funName: String = "get", block: (P1, P2, P3, P4) -> R) = GetOp4(lock, default, funName, block)
-    fun <P1, P2, P3, P4, P5, R> get(default: R, funName: String = "get", block: (P1, P2, P3, P4, P5) -> R) = GetOp5(lock, default, funName, block)
-    fun <P1, P2, P3, P4, P5, P6, R> get(default: R, funName: String = "get", block: (P1, P2, P3, P4, P5, P6) -> R) = GetOp6(lock, default, funName, block)
-    fun <P1, P2, P3, P4, P5, P6, P7, R> get(default: R, funName: String = "get", block: (P1, P2, P3, P4, P5, P6, P7) -> R) = GetOp7(lock, default, funName, block)
-    fun <P1, P2, P3, P4, P5, P6, P7, P8, R> get(default: R, funName: String = "get", block: (P1, P2, P3, P4, P5, P6, P7, P8) -> R) = GetOp8(lock, default, funName, block)
-    fun <P1, P2, P3, P4, P5, P6, P7, P8, P9, R> get(default: R, funName: String = "get", block: (P1, P2, P3, P4, P5, P6, P7, P8, P9) -> R) = GetOp9(lock, default, funName, block)
-    fun <P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, R> get(default: R, funName: String = "get", block: (P1, P2, P3, P4, P5, P6, P7, P8, P9, P10) -> R) = GetOp10(lock, default, funName, block)
 }
