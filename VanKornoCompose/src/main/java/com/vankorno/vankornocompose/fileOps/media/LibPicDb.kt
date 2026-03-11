5e7730844fb31a3168691ab2cb2146bc53bd672b
VanKorno <71512024+VanKorno@users.noreply.github.com>
2026-03-11 08:25:46 +0200

    LibPicDb

--- VanKornoCompose/src/main/java/com/vankorno/vankornocompose/fileOps/media/LibPicDb.kt
+++ VanKornoCompose/src/main/java/com/vankorno/vankornocompose/fileOps/media/LibPicDb.kt
@@ -1,16 +1,20 @@
 package com.vankorno.vankornocompose.fileOps.media
 
+import com.vankorno.vankornocompose._entities.pic.CPic.BecameUnused
 import com.vankorno.vankornocompose._entities.pic.CPic.Path
 import com.vankorno.vankornocompose._entities.pic.CPic.PreviewPath
+import com.vankorno.vankornocompose._entities.pic.CPic.Usages
 import com.vankorno.vankornocompose._entities.pic.PicEntt
 import com.vankorno.vankornocompose._entities.pic.TTTPic
 import com.vankorno.vankornocompose._entities.pic.getPic
 import com.vankorno.vankornocompose.fileOps.LibFileOps
 import com.vankorno.vankornodb.api.DbRuntime.dbh
 import com.vankorno.vankornohelpers.dLog
+import com.vankorno.vankornohelpers.getCurrTime
 
 object LibPicDb {
     private const val TAG = "LibPicDb"
+    private const val DaysTillDelete = 10
     
     fun getPic(id: Int ) = dbh.read(PicEntt(), "getPic") { db -> db.getPic(id) }
     
@@ -39,7 +43,18 @@ object LibPicDb {
     
     
     
-    
-    
+    fun cleanupPic() {
+        // region LOG
+            dLog(TAG, "cleanupPic()")
+        // endregion
+        val millisTillDelete = DaysTillDelete * 24 * 60 * 60 * 1000L
+        val delLine = getCurrTime() - millisTillDelete
+        
+        dbh.deleteRows(TTTPic) {
+            Usages less 1
+            and { BecameUnused less delLine }
+        }
+        deleteOrphanPicFiles()
+    }
     
 }
