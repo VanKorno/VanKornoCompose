1da0e28ae2046720216872a5daceb99878e0c98e
VanKorno <71512024+VanKorno@users.noreply.github.com>
2026-03-13 01:04:03 +0200

    update

--- VanKornoCompose/src/main/java/com/vankorno/vankornocompose/db/miscTable/MiscTableOps.kt
+++ VanKornoCompose/src/main/java/com/vankorno/vankornocompose/db/miscTable/MiscTableOps.kt
@@ -4,21 +4,14 @@ import com.vankorno.vankornocompose.db.LibDbVals.AppFirstLaunched
 import com.vankorno.vankornocompose.db.LibDbVals.TTTMisc
 import com.vankorno.vankornocompose.db.miscTable.CMisc.Long1
 import com.vankorno.vankornodb.api.DbRuntime.dbh
-import com.vankorno.vankornohelpers.getCurrTime
-import com.vankorno.vankornohelpers.values.LibConstants.MillisInDay
+import com.vankorno.vankornohelpers.getDaysTillNow
 
 object MiscTableOps {
     
     fun getDaysSinceFirstAppLaunch(): Int {
-        val currTime = getCurrTime()
         val firstLaunchTime = dbh.getLong(TTTMisc, Long1) { Name = AppFirstLaunched }
         
-        val diff = currTime - firstLaunchTime
-        
-        if (diff < MillisInDay)
-            return 0
-        
-        return (diff / MillisInDay).toInt()
+        return firstLaunchTime.getDaysTillNow()
     }
     
 }
