2ae6fc5906735199bb482521396edf23782e91bb
VanKorno <71512024+VanKorno@users.noreply.github.com>
2026-03-19 06:29:25 +0200

    Fix migrations

--- VanKornoCompose/src/main/java/com/vankorno/vankornocompose/_entities/pic/Migrations.kt
+++ VanKornoCompose/src/main/java/com/vankorno/vankornocompose/_entities/pic/Migrations.kt
@@ -3,12 +3,11 @@ package com.vankorno.vankornocompose._entities.pic
 import com.vankorno.vankornocompose._entities.pic.old.to100.to10._V1_Pic
 import com.vankorno.vankornocompose._entities.pic.old.to100.to10._V2_Pic
 import com.vankorno.vankornocompose._entities.pic.old.to100.to10._V3_Pic
-import com.vankorno.vankornocompose._entities.pic.old.to100.to10._V4_Pic
 import com.vankorno.vankornodb.api.defineMigrations
 
 fun migrationsPic() = defineMigrations(PicMeta) {
     version(1, _V1_Pic)
     version(2, _V2_Pic)
     version(3, _V3_Pic)
-    version(4, _V4_Pic)
+    version(4, _Pic)
 }
--- VanKornoCompose/src/main/java/com/vankorno/vankornocompose/_entities/usage/Migrations.kt
+++ VanKornoCompose/src/main/java/com/vankorno/vankornocompose/_entities/usage/Migrations.kt
@@ -1,8 +1,7 @@
 package com.vankorno.vankornocompose._entities.usage
 
-import com.vankorno.vankornocompose._entities.usage.old.to100.to10._V1_Usage
 import com.vankorno.vankornodb.api.defineMigrations
 
 fun migrationsUsage() = defineMigrations(UsageMeta) {
-    version(1, _V1_Usage)
+    version(1, _Usage)
 }
